c56660bc3ab031fe624ec3eace4f200a204e7721
Jakub Arnold <darthdeus@gmail.com>
2012-09-30 13:34:12 +0200

    Add Repo abstraction

--- app.rb
+++ app.rb
@@ -10,19 +10,50 @@ def repo_name(repo_url)
   $1
 end
 
-def clone_repo(repo)
-  FileUtils.rm_rf("tmp/repo")
-  grit = Grit::Git.new("/tmp/filler")
-  grit.clone({}, repo, "tmp/#{repo_name(repo)}")
-  "cloned into #{repo_name(repo)}"
+class Repo
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def clone(target_dir)
+    clean_previous_clone(target_dir)
+
+    grit = Grit::Git.new("/tmp/filler")
+    grit.clone({}, url, "#{target_dir}/#{name}")
+  end
+
+  def build(target_dir)
+    `cd #{target_dir}/#{name} && rake`
+  end
+
+  def name
+    @url =~ /.*\/([\w-]+)/
+    return $1
+  end
+
+  def clean_previous_clone(target_dir)
+    FileUtils.rm_rf("#{target_dir}/#{name}")
+  end
 end
 
-def build_repo(repo)
-  `cd tmp/#{repo_name(repo)} && rake`
+def clone_repo(repo_url)
+  repo = Repo.new(repo_url)
+  repo.clone("tmp")
+  "cloned into #{repo.name}"
+end
+
+def build_repo(repo_url)
+  repo = Repo.new(repo_url)
+  repo.build("tmp")
 end
 
 get "/" do
   options["repo"]
+
+  "<form action='/clone' method='post'><input type='submit' value='Clone'></form>" +
+  "<form action='/build' method='post'><input type='submit' value='Build'></form>"
 end
 
 post "/clone" do
